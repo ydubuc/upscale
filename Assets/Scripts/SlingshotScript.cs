bdf00b04f80d698d53d3bef7a621bf0bd0e2f509
Yoan <69927862+ydubuc@users.noreply.github.com>
2020-10-09 16:43:32 -0400

    Add Slingshot Script to player

---
+++ Assets/Scripts/SlingshotScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingshotScript : MonoBehaviour {
+    [SerializeField] Rigidbody player = default;
+
+    Vector3 touchOrigin;
+    Vector3 touchEnd;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ListenForInputs();
+    }
+
+    void ListenForInputs() {
+        if (Input.touchCount < 1) { return; }
+
+        Touch touch = Input.touches[0];
+
+        if (touch.phase == TouchPhase.Began) {
+            touchOrigin = touch.position;
+        } else if (touch.phase == TouchPhase.Ended && touchOrigin.x >= Mathf.Epsilon) {
+            touchEnd = touch.position;
+            Vector3 jumpForce = touchOrigin - touchEnd;
+
+            // transform.parent = null;
+            // player.constraints = RigidbodyConstraints.None;
+            // player.constraints = RigidbodyConstraints.FreezeRotation;
+
+            player.AddForce(Vector3.ClampMagnitude(jumpForce, 1100));
+        }
+    }
+}
