e2bdba11679dfa0cec3b055973baf3253b04cf46
Yoan <69927862+ydubuc@users.noreply.github.com>
2020-12-01 16:38:53 -0500

    add score text view, updated textures

---
+++ Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTracker : MonoBehaviour {
+    // dependencies
+    [SerializeField] Transform player = default;
+    [SerializeField] Text textViewScore = default;
+
+    // runtime variables
+    private float highestPos = 0f;
+
+    // functions
+    void Start() {
+        UpdateScore();
+    }
+
+    void Update() {
+        if (player.transform.position.y > highestPos) {
+            highestPos = player.transform.position.y;
+            UpdateScore();
+        }
+    }
+
+    private void UpdateScore() {
+        textViewScore.text = Mathf.FloorToInt(highestPos).ToString();
+    }
+}
